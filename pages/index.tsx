b70d2c83eb098bd6b1f5a346b3bab56276a5198e
Evan Prenger <eprenger23@gmail.com>
2022-02-23 12:28:53 -0500

    homepage post styling

--- pages/index.tsx
+++ pages/index.tsx
@@ -32,17 +32,17 @@ export default function Home({posts}: Props) {
 
 
       {/* Posts */}
-      <div>
+      <div className='grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-3 md:gap-6 p-2 md:p-6'>
         {posts.map(post => (
           <Link key={post._id} href={`/post/${post.slug.current}`}>
-          <div>
-            <img src={
+          <div className='border rounded-lg group cursor-pointer overflow-hidden'>
+            <img className='border rounded-lg h-60 w-full object-cover group-hover:scale-105 transition transform duration-200 ease-in-out' src={
               urlFor(post.mainImage).url()!
             } alt="post image" />
             <div className='flex justify-between p-5 bg-white'>
               <div>
-                <p>{post.title}</p>
-                <p>{post.description} by {post.author.name}</p>
+                <p className='font-lg font-bold'>{post.title}</p>
+                <p className='text-xs'>{post.description} by {post.author.name}</p>
               </div>
               <img className='h-12 w-12 rounded-full' src={urlFor(post.author.image).url()!} alt="author image" />
               </div>
