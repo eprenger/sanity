b489be4f16bae73d0b86636e624a9cbc8cce3e3d
Evan Prenger <eprenger23@gmail.com>
2022-02-23 14:51:46 -0500

    style post pages / implementing comments

--- pages/index.tsx
+++ pages/index.tsx
@@ -36,7 +36,7 @@ export default function Home({posts}: Props) {
         {posts.map(post => (
           <Link key={post._id} href={`/post/${post.slug.current}`}>
           <div className='border rounded-lg group cursor-pointer overflow-hidden'>
-            <img className='border rounded-lg h-60 w-full object-cover group-hover:scale-105 transition transform duration-200 ease-in-out' src={
+            <img className='h-60 w-full object-cover group-hover:scale-105 transition transform duration-200 ease-in-out' src={
               urlFor(post.mainImage).url()!
             } alt="post image" />
             <div className='flex justify-between p-5 bg-white'>
